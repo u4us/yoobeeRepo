b3960dd7d9622e2ae1b4e0351bbd98f6639e450a
Logan Pun <270021661@cit3-501m-03.local>
2019-05-09 12:21:14 +1200

    added events ex

---
+++ wk13/Events/Event_06_Stars/javascript.js
@@ -0,0 +1,9 @@
+function makeStars(iNum){
+	var sStars = "";
+	for(var iCount=0; iCount<iNum; iCount++){
+		sStars += "*";
+	}
+	return sStars;//string of stars
+}
+
+//---------------------------------------------
