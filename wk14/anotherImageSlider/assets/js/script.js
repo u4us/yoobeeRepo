93173ef06ad9c90bf746ee5ac221173be43d468f
Logan <lpun004@aucklanduni.ac.nz>
2019-05-15 11:54:10 +1200

    changes to ex

--- wk14/anotherImageSlider/assets/js/script.js
+++ wk14/anotherImageSlider/assets/js/script.js
@@ -1,1 +1,23 @@
 //https://coolcarousels.frebsite.nl/c/71/
+
+function moveNext(){
+    var current = document.querySelector('.current');
+    current.classList.remove('current');
+
+    if(current.nextElementSibling != null){
+        current.nextElementSibling.classList.add('current');
+    }else{
+        current.parentNode.firstElementChild.classList.add('current');
+    }
+};
+
+function movePrev(){
+
+};
+
+var right = document.querySelector('.fa-chevron-circle-right');
+right.addEventListener('click', moveNext);
+
+var left = document.querySelector('.fa-chevron-circle-left');
+left.addEventListener('click', movePrev);
+//assigning right images
