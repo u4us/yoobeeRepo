744e661064ca07322bcf41e59807aa2af64bd680
Logan Pun <270021661@cit3-501m-03.local>
2019-05-14 15:28:50 +1200

    added another slider

--- wk14/ImageSlider_Students/assets/js/script.js
+++ wk14/ImageSlider_Students/assets/js/script.js
@@ -1,2 +1,32 @@
+function moveNext(){
+    console.log('but where will it take me? on...');
 
+    var current = document.querySelector('.current');
+    current.classList.remove('current');
+    
+    if(current.nextElementSibling != null){
+        current.nextElementSibling.classList.add('current');
+    } else{
+        current.parentNode.firstElementChild.classList.add('current');
+    }
+}
 
+function movePrev(){
+    var current = document.querySelector('.current');
+    current.classList.remove('current');
+
+    if(current.previousElementSibling != null){
+        current.previousElementSibling.classList.add('current');
+    } else{
+        current.parentNode.lastElementChild.classList.add('current')
+    }
+}
+
+var right = document.querySelector('.image-slider .fa-chevron-circle-right');
+//make sure to target correct chevron
+right.addEventListener('click', moveNext);
+
+var left = document.querySelector('.fa-chevron-circle-left');
+left.addEventListener('click',movePrev);
+
+setInterval(moveNext, 3000);
---
+++ wk14/anotherImageSlider/assets/js/script.js
@@ -0,0 +1,1 @@
+//https://coolcarousels.frebsite.nl/c/71/
