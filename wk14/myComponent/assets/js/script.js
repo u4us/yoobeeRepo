0187840af10bdb1e963a39d8d2e8e8cb501c3981
Logan <lpun004@aucklanduni.ac.nz>
2019-05-20 12:04:44 +1200

    completed component

--- wk14/myComponent/assets/js/script.js
+++ wk14/myComponent/assets/js/script.js
@@ -5,10 +5,26 @@ function show(){
 
     var targetSlide = document.querySelector(this.dataset.target);
     targetSlide.classList.add('current');
+
+    //triangles
+    var currentTriangle = document.querySelector('.current-triangle');
+    currentTriangle.classList.remove('current-triangle');
+
+    var targetTriangle = document.querySelector(this.dataset.triangle);
+    targetTriangle.classList.add('current-triangle');
+    //action
+    var aTriangle = document.querySelector('.triangles').children[0];
+    aTriangle.className = 'triangle';
+
+    var targetAction = document.querySelector(this.dataset.action);
+    document.querySelector('.triangle').classList.add(targetAction);
+
     //sidebar
     var currentItem = document.querySelector('.current-item');
     currentItem.classList.remove('current-item');
     this.classList.add('current-item');
+
+
 }
 
 function moveNext(){
@@ -34,4 +50,4 @@ for (let i = 0; i < items.length; i++) {
     items[i].addEventListener('click',show);
 }
 
-setInterval(moveNext, 5000);
+// setInterval(moveNext, 5000);
