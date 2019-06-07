80058e63967e91c974d9595fec1abae2ca584cc1
Logan Pun <270021661@cit3-501m-03.local>
2019-06-07 14:49:37 +1200

    more parallax

--- wk17/practice_advert/assets/script.js
+++ wk17/practice_advert/assets/script.js
@@ -1,6 +1,21 @@
 $(function(){
-    var timeline = anime({
+    var timeline = anime.timeline({
         duration: 3000,
-        
+        easing: 'linear',
+
+    });
+
+    timeline.add({
+        targets: '.motion1 .someBox',
+        translateX: ['250']
+    }).add({
+        targets: '.motion1 .someBox',
+        translateY: ['250']
+    }).add({
+        targets: '.motion1 .someBox',
+        translateX: ['250', 0]
+    }).add({
+        targets: '.motion1 .someBox',
+        translateY: ['250', 0]
     });
 });
