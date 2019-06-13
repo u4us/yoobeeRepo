81526a1e087e2db30b67901430c5cf763070685c
Logan <lpun004@aucklanduni.ac.nz>
2019-06-13 12:29:53 +1200

    updated formative

--- wk18/formative2.2/assets/script.js
+++ wk18/formative2.2/assets/script.js
@@ -6,14 +6,34 @@ $(function(){
     var tl1 = anime.timeline({
         easing: 'linear',
         autoplay: false
-    }).add({
+    })
+    .add({
         targets: '.section1 .layer5',
         translateX: ['100vw','-50vw']
-    }).add({
+    },500)
+    .add({
+        targets: '.section1 .layer6',
+        translateX: ['100vw','-50vw']
+    },1000)
+    .add({
+        targets: '.section1 .layer7',
+        translateX: ['100vw','-50vw']
+    },1500)
+    .add({
+        targets: '.section1 .layer8',
+        translateX: ['100vw','-100vw']
+    },3000)
+    .add({
+        targets: '.section1 .layer9',
+        translateY: ['-100vh',0]
+    },3500)
+    .add({
         targets: '.section1 .viewport',
         translateY: [0,'1000vh'],
-        duration: 1000
+        duration: 4500
     },0);
+
+    console.log(tl1.duration);
 
     var screenHeight = $(window).height();
     var section1Offset = $('.section1').offset().top;
@@ -21,5 +41,12 @@ $(function(){
         var scrollTop = $(document).scrollTop();
         var progress = (scrollTop-section1Offset)/(screenHeight*10);
         tl1.seek(tl1.duration*progress);
+    });
+
+    $('[data-target]').on('click', function(){
+        $($(this).data('target')).addClass('animated swing');
+        $($(this).data('target')).one('animationend', function(){
+            $(this).removeClass('swing').addClass('invisible');
+        });
     });
 });
